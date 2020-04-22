d5b072991246ffdef7763e9d31d73f91ddaa3d46
albertbrook <3226521643@qq.com>
2020-04-22 21:56:49 +0800

    add Main.java

---
+++ Main.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Main extends JFrame {
+
+    private Main() {
+        setTitle("Music Box - AlbertBrook");
+        setResizable(false);
+        setSize(Settings.SCREEN_SIZE);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        add(Player.getPlayer());
+        Functions.getFunctions().event();
+    }
+
+    public static void main(String[] args) {
+        new Main().setVisible(true);
+    }
+}
