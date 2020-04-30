a25ebc8833229df6e2fc7ef5378965dd03f22a72
albertbrook <3226521643@qq.com>
2020-04-30 23:38:56 +0800

    Update Functions.java

--- Functions.java
+++ Functions.java
@@ -1,14 +1,10 @@
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.*;
 
 class Functions {
-
     private static Functions functions;
 
     private Player player;
-
-    private Thread play;
+    private Play play;
 
     static Functions getFunctions() {
         if (functions == null) {
@@ -19,20 +15,40 @@ class Functions {
 
     private Functions() {
         player = Player.getPlayer();
+        play = Play.getPlay(Settings.track1, Settings.track2);
 
-        play = new Play();
+        play.start();
     }
 
     void event() {
         MouseListener mouseListener = new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                play.start();
-            }
+            private boolean loop;
 
             @Override
-            public void mouseReleased(MouseEvent e) {
-//                play.interrupt();
+            public void mousePressed(MouseEvent e) {
+                int index = player.getButton(e.getPoint());
+                if (index != -1)
+                {
+                    player.mouseDown(index);
+                    switch (index) {
+                        case 0:
+                            player.mouseUp(1);
+                            play.playStart();
+                            break;
+                        case 1:
+                            player.mouseUp(0);
+                            play.playStop();
+                            break;
+                        case 2:
+                            loop = !loop;
+                            if (loop)
+                                player.mouseDown(2);
+                            else
+                                player.mouseUp(2);
+                            play.playLoop(loop);
+                            break;
+                    }
+                }
             }
         };
         player.addMouseListener(mouseListener);
